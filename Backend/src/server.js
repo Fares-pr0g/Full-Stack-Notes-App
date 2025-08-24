c5dc376be8cbb372ba744372d1b3b13da93468a8
Fares-pr0g <elabed.fares.2004@gmail.com>
2025-08-24 14:33:46 +0100

    preparation for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,21 +4,34 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import router from './routes/notesRoutes.js';
+import path from "path";
 
 dotenv.config();
 
 const app= express();
 const PORT = process.env.PORT || 5001;
+const __dirname= path.resolve();
 
 
 // middleware
+if (process.env.NODE_ENV !== "production") {    
+    app.use(cors({
+        origin:"http://localhost:5173"
+    }));
+};
 
-app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
 
 app.use('/api/fares/notes', router)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    })
+};
 
 connectDB().then(() => {
     app.listen(PORT, (req,res) => {
